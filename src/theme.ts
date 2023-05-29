67ce84cc5f6f40b319e6cf37d6c51b0ac5e8e665
Justin Forlenza <justin@forlenza.co>
2023-05-29 10:19:05 -0400

    Theme tweaks

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { type GlobalThemeOverrides } from 'naive-ui'
+
+export const themeOverrides: GlobalThemeOverrides = {
+  common: {
+    fontSizeHuge: '20px',
+    fontSizeLarge: '18px',
+    fontSizeSmall: '16px',
+    fontSizeMedium: '16px',
+    fontSize: '16px',
+    fontSizeTiny: '14px',
+    fontSizeMini: '14px'
+  },
+  Message: {
+    fontSize: '18px'
+  }
+}
